7df9d838e173930bf943c9091b49fca30b98ff4c
alliegryboski04 <allie.gryboski@tcu.edu>
2026-04-07 13:07:42 -0500

    Update feature_utils.py

--- src/feature_utils.py
+++ src/feature_utils.py
@@ -95,7 +95,7 @@ def convert_input_pca_regression(request_body, request_content_type):
 
     dataset = pd.read_csv(file_path,index_col=0)
 
-    target = 'MSFT'
+    target = 'WMT'
 
     option = 2
 
@@ -110,15 +110,15 @@ def convert_input_pca_regression(request_body, request_content_type):
 
         # Calculate the distance
         distances = np.sqrt(
-            (X[techIndicator_1] - RSI_15)**2 + 
-            (X[techIndicator_2] - MOM_15)**2
+            (X[techIndicator_1] - EMA_15)**2 + 
+            (X[techIndicator_2] - EMA_10)**2
         )
         
         closest_index = distances.idxmin()
         closest_row = X.loc[[closest_index]]
     
-        closest_row[techIndicator_1] = RSI_15
-        closest_row[techIndicator_2] = MOM_15
+        closest_row[techIndicator_1] = EMA_15
+        closest_row[techIndicator_2] = EMA_10
     
         return closest_row
     else:
